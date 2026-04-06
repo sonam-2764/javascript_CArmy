5004e28db9c9c889ad62dd3b6deab29256fa763d
Sonam Kumari <aspritualgirl@gmail.com>
2026-04-06 14:45:19 +0530

    substring and concatenation

--- day.js/day4.js
+++ day.js/day4.js
@@ -26,3 +26,24 @@
 // console.log(str1.indexOf('Cod'));
 // console.log(str1.lastIndexOf('Cod'));
 // console.log(str1.includes('Cod'));
+
+// //slice // cuts and extracts the part of the string
+// console.log(str.slice(2,5));
+// console.log(str.slice(2,7));
+// console.log(str.slice(3));//cuts the strings from index 3 and gives that as output
+// console.log(str.slice(-4));//cuts the string at last 4 position and gives that as output ...ulta dekhta h
+// console.log(str.slice(-5,-2));
+
+// //SUBSTRING //we can't mark -ve index
+// const str = `Hello Coder Army Coder`;
+// console.log(str.substring(2,5));
+
+
+// //CONCATENATING STRINGS
+// const a ="Sonam";
+// const b = "Kumari";
+// const c = a+" "+b;
+// console.log(c);
+// console.log(24+ "Sonam");//convets 24 to string and concatenate 24 and sonam
+// console.log(24+"Sonam"+10);//similarly converts 24 and 10 to string
+// console.log(24 + 30 +"Sonam");//adds 24 and 30 and convetrs their sum as string and concatenate them
