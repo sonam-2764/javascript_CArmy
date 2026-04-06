5e5041e548ae0c870ada37f606d17f06db6249d9
Sonam Kumari <aspritualgirl@gmail.com>
2026-04-06 14:22:22 +0530

    finding substring

--- day.js/day4.js
+++ day.js/day4.js
@@ -5,9 +5,24 @@
 // const str3 =`Strike is coming on ${day}`;//learnt about this symbol in jonas course
 // console.log(str1,str2,str3);
 
-const str = `Hello Coder army`;
-console.log(str.length);
-console.log(str[2]);
+// const str = `Hello Coder army`;
+// console.log(str.length);
+// console.log(str[2]);
 
-str[2]="s";
-console.log(str);//ndoesn't change because string is primitive data type and that is immutable
+// str[2]="s";
+// console.log(str);//ndoesn't change because string is primitive data type and that is immutable
+
+// //CONVERSION TO UPPER CASE
+// console.log(str.toUpperCase());
+// console.log(str.toLoweCase());
+
+// //FINDING SUBSTRING
+// const str = `Hello Coder Army`;
+// console.log(str.indexOf('Cer'));//it will return -1 ....-1 is returned when either the data isn't found or aren't together
+// console.log(str.indexOf('Cod'));
+// // cases issues like lowercase and uppercase may result in data not found so we first convert all the code in uppercase then continue
+// //if anything is present in multiple places ...it will return the first index
+// const str1 = `Hello Coder Army Coder`;
+// console.log(str1.indexOf('Cod'));
+// console.log(str1.lastIndexOf('Cod'));
+// console.log(str1.includes('Cod'));
