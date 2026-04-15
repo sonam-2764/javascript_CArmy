4d7b8b5ae5a00f461896f94f935f638de7f6fdd2
Sonam Kumari <aspritualgirl@gmail.com>
2026-04-15 07:39:45 +0530

    declaring and calling function

---
+++ day.js/day7.js
@@ -0,0 +1,32 @@
+//FUNCTION
+
+// // //declaring function
+//  function greeting(){
+//     console.log("hello there, I am 20 years old");
+//  }
+
+//  // calling function
+//  greeting();
+  
+//  //function is created for reusability
+//  function addNumber(num1,num2){
+//     const sum = num1 + num2;
+//     console.log(sum);
+//  }
+//  addNumber(3,4); // 3 and 4 are arguments and num1 and num2 are parameters
+//  addNumber(6,7);
+
+//  greeting(); //calls the function and gives undefined in output because no return value is assigned 
+// function greeting(){
+//     console.log("hello there, I am 20 years old");
+//     return 10;
+//  }
+
+// Suppose a function with variable parameters
+// function addNumber(num1,num2,num3 = 0){
+//     const sum = num1 + num2;
+//     console.log(sum);
+//  }
+//  addNumber(3,4);
+//  addNumber(6,7,8);
+// //this ensures that when only 2 numbers are given take default value of 3rd number that is 0
