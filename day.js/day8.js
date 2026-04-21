2d927f42ace2a76f3d1cbb511179b3acd915c0db
Sonam Kumari <aspritualgirl@gmail.com>
2026-04-21 15:10:38 +0530

    definition

--- day.js/day8.js
+++ day.js/day8.js
@@ -14,3 +14,5 @@
 //function works before execution phase because function code is already present as during memory allocation memory is allocated to function and function code is also presengt there
 
 //const wale ke saath kuch bhi temporal dead zone me jayega
+
+//hoisting in js is a behaviour of knowing about a variable or function's existence before executing the code ..How it treats the knowledge depends upon the keyword function,var,let or const.
