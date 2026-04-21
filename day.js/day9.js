1969e0e5aeb79a9788cd729c0950aa3dfa7d3a4b
Sonam Kumari <aspritualgirl@gmail.com>
2026-04-21 15:30:06 +0530

    function

--- day.js/day9.js
+++ day.js/day9.js
@@ -1,15 +1,31 @@
-// //CLOSURE
-let a =10;
-const b =20;
-if(true){
-    console.log(b);//can be accessed everywhere
-}
-function greet(){
-    console.log(a);
-    let c =30;//can be accessed only inside the function
-}
-greet();
-//scope is of three types --- global and functional and block level scope
-//global can be accessed anywhere in the code 
-//function can be accessed only within the function
-//block can be accessed only to that block
+// // //CLOSURE
+// let a =10;
+// const b =20;
+// if(true){
+//     console.log(b);//can be accessed everywhere
+// }
+// function greet(){
+//     console.log(a);
+//     let c =30;//can be accessed only inside the function
+// }
+// greet();
+// //scope is of three types --- global and functional and block level scope
+// //global can be accessed anywhere in the code 
+// //function can be accessed only within the function
+// //block can be accessed only to that block
+
+
+// let global =30;
+// function greet(){
+//     let global =40;//no errors as this global is inside greet function
+//     function meet(){
+//         let global = 10;
+//         console.log(global);//no error
+//     }
+//     meet();
+// }
+// greet();
+
+// //agar kbhi andar vo function na ho toh pehle vo apne aas paas k block me vo variable dhundega jahan value dikhi print
+// //LET supports global and functional but not block
+
