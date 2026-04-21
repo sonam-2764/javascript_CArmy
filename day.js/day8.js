0225ca03d426a3de715fdce9cee7c74a3ccda64c
Sonam Kumari <aspritualgirl@gmail.com>
2026-04-21 14:25:27 +0530

    hoisting

--- day.js/day8.js
+++ day.js/day8.js
@@ -1,1 +1,7 @@
 // //HOISTING
+// code run --> execution context 
+// 1.Memory allocation
+// 2.Execution phase
+// // even when a number is called these two phases again take place when a function is called 
+//Primitive data in stack and rest in heap //memory allocation
+//GEC: global execution context
