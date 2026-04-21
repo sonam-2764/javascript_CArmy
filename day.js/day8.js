a9adf535caab35d6bb0703a3cf4d2b7a8800445f
Sonam Kumari <aspritualgirl@gmail.com>
2026-04-21 15:07:53 +0530

    how code works

--- day.js/day8.js
+++ day.js/day8.js
@@ -5,3 +5,12 @@
 // // even when a number is called these two phases again take place when a function is called 
 //Primitive data in stack and rest in heap //memory allocation
 //GEC: global execution context
+
+//temporal dead zone(cannot be accessed without initialization)
+//with let and const
+
+//for var it shows undefined if called before initialization
+
+//function works before execution phase because function code is already present as during memory allocation memory is allocated to function and function code is also presengt there
+
+//const wale ke saath kuch bhi temporal dead zone me jayega
