808613e83f75a61707934b75e3afa5195efeb74b
Sonam Kumari <aspritualgirl@gmail.com>
2026-04-23 16:26:07 +0530

    filter

--- day.js/day10.js
+++ day.js/day10.js
@@ -10,7 +10,6 @@
 // }) 
 
 
-
 // const arr = [10,20,30,40,90];
 // let sum =0;
 // arr.forEach((number, index ,arr)=>{//this can take upto 3 parameter
@@ -18,3 +17,11 @@
 // sum+=number;
 // })
 // console.log(sum);
+
+
+
+// //FILTER : It is used to select specific elements from the array 
+const arr = [10,20,30,50,70];
+// arr.filter((number) => number >25); // this returns a new array
+const newArr = arr.filter((number) => number >25);
+console.log(newArr);
