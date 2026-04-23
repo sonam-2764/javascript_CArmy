ae26b77570966d2633dc7296d97fc8be8a27bcb5
Sonam Kumari <aspritualgirl@gmail.com>
2026-04-23 16:16:16 +0530

    for each loop

---
+++ day.js/day10.js
@@ -0,0 +1,20 @@
+// //FOR EACH, FILTER, REDUCER, MAP, SET
+
+
+
+// // FOR EACH
+// const arr = [10,20,30,"Sonam",90];
+// //for each is used to iterating an array
+// arr.forEach((number) =>{
+//     console.log(number);
+// }) 
+
+
+
+// const arr = [10,20,30,40,90];
+// let sum =0;
+// arr.forEach((number, index ,arr)=>{//this can take upto 3 parameter
+//     console.log(number,index,arr);
+// sum+=number;
+// })
+// console.log(sum);
