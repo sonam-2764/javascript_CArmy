2d4fdf13a3c4728472c51285d2d33c2354c1e0d6
Sonam Kumari <aspritualgirl@gmail.com>
2026-04-27 22:55:20 +0530

    prototype

--- day.js/day10.js
+++ day.js/day10.js
@@ -26,7 +26,7 @@
 // const newArr = arr.filter((number) => number >25);
 // console.log(newArr);
 
-// Array.filtering = function(compare){
+// arr.filtering = function(compare){
 //     const ans =[];
 //     for(let num of arr){//can write 'this' word in place of arr
 //         if(compare(num)){
@@ -37,3 +37,26 @@
 // }
 // const newArr=arr.filtering((num)=> num >25);
 // console.log(newArr);
+
+
+
+// // //REUSABLE METHOD WITH PROTOTYPE
+// // // if we create a new array we won't be able to use the filtering property as it was custom method for that only
+// // const a = [80,30,15,31,42,70]
+// // const answer = a.filtering((num)=> num>25);
+// // console.log(answer); //this will throw an error as it was a custom methid for that particular array
+// // //to over come this we need to change the property of original array
+// // //in line 29 instead of arr.filtering = function(compare) we write
+// Array.prototype.filtering= function(compare){
+//     const ans =[];
+//     for(let num of arr){//can write 'this' word in place of arr
+//         if(compare(num)){
+//             ans.push(num);
+//         }
+//     }
+//     return ans;
+// }
+// const newArr=arr.filtering((num)=> num >25);
+// console.log(newArr);
+
+// //now for any number of array no error
