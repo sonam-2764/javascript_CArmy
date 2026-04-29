f3bb1ab2ce268efc572234f19b4bed37136d7aad
Sonam Kumari <aspritualgirl@gmail.com>
2026-04-29 07:32:15 +0530

    learning

--- day.js/day11.js
+++ day.js/day11.js
@@ -1,2 +1,21 @@
-//DOM : Document objeect model
+//DOM : Document object model
+// with the help of js we can change html file: js just converts the properties into an object making it easy to change things 
+
+//browser creates-- windows --> Document --> html
+//windows is a global object
+//<h2 id="second"> It is coming on dhanteras</h2>
+//it is stored as 
+//const temp ={
+// tagName:"h2",
+// id:"second",
+// textContent:"It is coming on dhanteras",
+// }
+//Suppose body has 2 elements or tags h1 and h2; it will create an object of body with all the properties then it will show children: and will create an array as number of tags can vary...--> children:[temp1, temp]; they are the object of each element
+
+
+
+
+//Windows.document.getElementById("title");
+//console.dir(document);
+//this helps to create an object of an element
 
