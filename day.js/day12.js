4b3b429a4dbd162b87707512311f96ef037e8b24
Sonam Kumari <aspritualgirl@gmail.com>
2026-05-03 00:29:53 +0530

    create

--- day.js/day12.js
+++ day.js/day12.js
@@ -13,7 +13,12 @@ element.after(newElement);
 const newElement2 = document.createElement('h3');
 newElement2.textContent = "I am 2nd year student";
 newElement2.id = "third";
-newElement2.className = "Student";
-newElement2.className = "Girl";
-//the 2nd code over ride the 1st line and ab class name girl dikhega student gayab
+//newElement2.className = "Student";
+// newElement2.className = "Girl";
+// //the 2nd code over ride the 1st line and ab class name girl dikhega student gayab
+// //newElement2.className += "Girl"; // ab class ki naam Student Girl dikhegi
+// // but if we want to assign both name to aa single class
+newElement2.classList.add("Student");
+newElement2.classList.add("Girl");
+ewElement2.classList.remove("Student");
 console.log(newElement2);
