400be1369e14d674d121e456d73b5db64772d27d
Sonam Kumari <aspritualgirl@gmail.com>
2026-05-04 23:33:12 +0530

    crud

--- day.js/day12.js
+++ day.js/day12.js
@@ -28,3 +28,55 @@ element.before(newElement2);
 element.before(newElement2.getAttribute("id"));
 element.before(newElement2.getAttribute("class"));
 element.before(newElement2.setAttribute("hello", "5"));
+
+
+// const list = document.createElement("li");
+// list.textContent = "Milk"
+// const list2 = document.createElement("li");
+// list2.textContent = "Cake"
+
+// const list3 = document.createElement("li");
+// list3.textContent = "Halwa"
+
+// const list4 = document.createElement("li");
+// list4.textContent = "Paneer"
+
+// const unorderElement = document.getElementById("listing");
+
+// unorderElement.append(list, list2);
+
+// unorderElement.prepend(list3);
+
+// unorderElement.children[1].after(list4);
+
+const arr = ["Milk", "Halwa", "Paneer","tofu","Tea"];
+
+const unorderElement = document.getElementById("listing");
+const fragment = document.createDocumentFragment();
+// array
+
+for(let food of arr){
+    const list = document.createElement("li");
+    list.textContent = food;
+    fragment.append(list);
+}
+
+unorderElement.append(fragment);
+
+const s1 = document.getElementById("first");
+s1.remove();
+
+
+const month = document.getElementById("ten");
+
+// console.log(month.children);
+const lister = document.createElement("li");
+lister.textContent = "<img src='https://plus.unsplash.com/premium_photo-1689530775582-83b8abdb5020?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fHww&fm=jpg&q=60&w=3000'>";
+
+// lister.innerHTML = "<img src='https://plus.unsplash.com/premium_photo-1689530775582-83b8abdb5020?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fHww&fm=jpg&q=60&w=3000'>"
+
+month.prepend(lister);
+// month.insertAdjacentElement("afterend",lister)
+
+
+
