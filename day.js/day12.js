aaa8d042c294670853752237993884f6135278b7
Sonam Kumari <aspritualgirl@gmail.com>
2026-05-05 13:27:41 +0530

    update

--- day.js/day12.js
+++ day.js/day12.js
@@ -15,7 +15,6 @@ newElement2.textContent = "I am 2nd year student";
 newElement2.id = "third";
 //newElement2.className = "Student";
 // newElement2.className = "Girl";
-// //the 2nd code over ride the 1st line and ab class name girl dikhega student gayab
 // //newElement2.className += "Girl"; // ab class ki naam Student Girl dikhegi
 // // but if we want to assign both name to aa single class
 newElement2.classList.add("Student");
