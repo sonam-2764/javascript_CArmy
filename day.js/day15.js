5a53bf75ff24886a6ad175e726a050270700c345
Sonam Kumari <aspritualgirl@gmail.com>
2026-05-25 13:40:06 +0530

    callback

--- day.js/day15.js
+++ day.js/day15.js
@@ -1,2 +1,20 @@
 // //CALLBACK HELL
-//it is t
+//
+//zomato application
+function placeOrder(callback){
+    console.log("Payment is in progress");
+
+    setTimeout(() => {
+        console.log("Payment is received and order get placed");
+        callback;
+},3000)
+}
+
+function preparingOrder(){
+    console.log("your food preparatin started");
+    setTimeout(()=>{
+        console.log("Your order is now prepared");
+    },3000);
+}
+placeOrder(preparingOrder);
+//preparingOrder();
