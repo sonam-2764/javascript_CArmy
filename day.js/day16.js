4a33f26c3d0b807530401e15170c859825bd5b1c
Sonam Kumari <aspritualgirl@gmail.com>
2026-05-28 14:10:58 +0530

    other method

--- day.js/day16.js
+++ day.js/day16.js
@@ -3,23 +3,30 @@
 
 console.log("Hello world Start");
 
-const p1 = fetch("https://api.github.com/users");
-//console.log(p1);
+// const p1 = fetch("https://api.github.com/users");
+// //console.log(p1);
 
-// //it only reads data  either if it is fulfilled or rejected
-// p1.then((response) => {
-//     console.log(response); //for converting it into js object write response.json()
+// // //it only reads data  either if it is fulfilled or rejected
+// // p1.then((response) => {
+// //     console.log(response); //for converting it into js object write response.json()
+// // })
+
+// const p2 = p1.then((response) => {
+//     return response.json();
 // })
-
-const p2 = p1.then((response) => {
-    return response.json();
-})
-p2.then((response) => {
-    console.log(response);
-})
+// p2.then((response) => {
+//     console.log(response);
+// })
 
 //promise is an object which has three stages : pending, fulfilled , reject
 
+fetch("https://api.github.com/users")
+.then((response) => {
+    return response.json();
+})
+.then((data)=> {
+    console.log(data);
+})
 
 
 
