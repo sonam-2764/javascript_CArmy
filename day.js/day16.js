af81966f0b4cbbaf8fb08aeb5fc1381656d6fac3
Sonam Kumari <aspritualgirl@gmail.com>
2026-06-29 15:05:16 +0530

    error

--- day.js/day16.js
+++ day.js/day16.js
@@ -22,37 +22,45 @@ console.log("Hello world Start");
 //promise is an object which has three stages : pending, fulfilled , reject
 
 // //2nd method
-// fetch("https://api.github.com/users")
-// .then((response) => {
-//     return response.json(); //display the data
-// })
-// .then((data)=> { // to call data
-//     //console.log(data);
+fetch("https://api.github.com/users")
+.then((response) => {
+    console.log(response);
+    if(!response.ok){
+        throw new Error("Data is not present in server");
+    }
+    return response.json(); //display the data
+})
+.then((data)=> { // to call data
+    //console.log(data);
 
 
-//     const parent = document.getElementById("first");
+    const parent = document.getElementById("first");
 
-//     for(let i =0; i<data.length;i++){
-//     const image = document.createElement('img');
-//     image.src = data[i].avatar_url;
-//     image.style.height = "40 px";
-//     image.style.width = "40 px";
+    for(let i =0; i<data.length;i++){
+    const image = document.createElement('img');
+    image.src = data[i].avatar_url;
+    image.style.height = "40 px";
+    image.style.width = "40 px";
 
-//     parent.append(image);
-//     }
-// })
+    parent.append(image);
+    }
+})
+.catch((error) => {
+    const parent = document.getElementById("first");
+    parent.textContent = error.message;
+})
+
+//console.log("Hello world End");
+
+//if server responds then promise is fulfilled but only if internet down,server down or DNS down them promise is rejected even if the program fails it is still fulfilled because server responded
+
+
+
 
 
 
 // //json vs js object
 // json - can't write keywords like undefined, function ; it is in string format; it is universal ie understands all language (C++, java etc)
-
-
-//console.log("Hello world End");
-
-
-
-
 
 //  const j1 = {
 //     name : "Rohit",
@@ -64,12 +72,12 @@ console.log("Hello world Start");
 //  console.log(jsonFormat);
 
 
-const jsonFormat = `{
-    "name": "rohit",
-    "age": 30,
-    "address": "dwarka"
-}`;// it is in string format
+// const jsonFormat = `{
+//     "name": "rohit",
+//     "age": 30,
+//     "address": "dwarka"
+// }`;// it is in string format
 
+// //convert to js object
+// const JsObject = JSON.parse(jsonFormat);
 
-//convert to js object
-const JsObject = JSON.parse(jsonFormat);
