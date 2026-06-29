176c9cbda0de5d5bd59fbbac8ad79994e17912cd
Sonam Kumari <aspritualgirl@gmail.com>
2026-06-29 14:36:15 +0530

    json vs js object

--- day.js/day16.js
+++ day.js/day16.js
@@ -11,6 +11,7 @@ console.log("Hello world Start");
 // //     console.log(response); //for converting it into js object write response.json()
 // // })
 
+// const p1 = fetch("https://api.github.com/users");
 // const p2 = p1.then((response) => {
 //     return response.json();
 // })
@@ -20,21 +21,55 @@ console.log("Hello world Start");
 
 //promise is an object which has three stages : pending, fulfilled , reject
 
-//2nd method
-fetch("https://api.github.com/users")
-.then((response) => {
-    return response.json();
-})
-.then((data)=> {
-    //console.log(data);
-    const parent = document.getElementById("first");
-    const image = document.createElement('img');
-    image.src = data[0].avatar_url;
-
-    parent.append(image);
-})
+// //2nd method
+// fetch("https://api.github.com/users")
+// .then((response) => {
+//     return response.json(); //display the data
+// })
+// .then((data)=> { // to call data
+//     //console.log(data);
 
 
+//     const parent = document.getElementById("first");
+
+//     for(let i =0; i<data.length;i++){
+//     const image = document.createElement('img');
+//     image.src = data[i].avatar_url;
+//     image.style.height = "40 px";
+//     image.style.width = "40 px";
+
+//     parent.append(image);
+//     }
+// })
 
 
-console.log("Hello world End");
+
+// //json vs js object
+// json - can't write keywords like undefined, function ; it is in string format; it is universal ie understands all language (C++, java etc)
+
+
+//console.log("Hello world End");
+
+
+
+
+
+//  const j1 = {
+//     name : "Rohit",
+//     address: "dwarka",
+//  }
+
+// //convert to json
+//  const jsonFormat = JSON.stringify(j1);
+//  console.log(jsonFormat);
+
+
+const jsonFormat = `{
+    "name": "rohit",
+    "age": 30,
+    "address": "dwarka"
+}`;// it is in string format
+
+
+//convert to js object
+const JsObject = JSON.parse(jsonFormat);
